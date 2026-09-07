af9d9db038ce6b10d58f15711a5fa66c1c9abe3f
secretjuzi <nxzhshd@icloud.com>
2026-09-08 01:23:59 +0800

    完成PermissionX库的开发与测试工作

--- app/src/main/java/com/permissionx/app/MainActivity.kt
+++ app/src/main/java/com/permissionx/app/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.permissionx.app
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.permissionx.juzidev.PermissionX
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        makeCallBtn.setOnClickListener {
+            PermissionX.request(this,
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_CONTACTS) { allGranted, deniedList ->
+                if (allGranted){
+                    call()
+                    Toast.makeText(this, "所有权限已被授权", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "you denied $deniedList", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
+    private fun call(){
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+    }
+
 }
